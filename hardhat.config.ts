19b912f53e378378879b7f93be6780b47d26c3da
Ismael Bejarano <ismaelbej@gmail.com>
2023-03-13 00:49:45 -0300

    Add configuration file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    localhost: {
+      url: process.env.NODE_URL,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+        path: "m/44'/60'/0'/0",
+        initialIndex: 0,
+        count: 20,
+        passphrase: "",
+      },
+    }
+  },
+  solidity: { 
+    version: "0.8.18", 
+  }
 };
 
 export default config;
